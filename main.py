170a3d3687ebaff3edd4ccbec87d98b6bc1fa3e7
정승환 <jeongseunghwan@jeongseunghwan-ui-MacBookAir.local>
2023-11-17 00:50:04 +0900

    logging api request

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 from common.response import get_reply
+import logging
 
 
 class Conversation(BaseModel):
     user_id: int
     sentence: str
     character_name: str
+
+    def __str__(self):
+        return 'user_id:%d sentence:%s character_name:%s' % (self.user_id, self.sentence, self.character_name)
 
 
 app = FastAPI()
@@ -20,6 +24,7 @@ def test():
 
 @app.post("/conversation")
 def get_response(conversation: Conversation):
+    logging.info('Request content: ' + conversation)
     res = get_reply(user_id=str(conversation.user_id),
-                    sentence=conversation.sentence,character_name=conversation.character_name)
+                    sentence=conversation.sentence, character_name=conversation.character_name)
     return {"text": res}
